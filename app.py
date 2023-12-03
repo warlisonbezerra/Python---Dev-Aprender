d75cedc53117180541c0a569cdde324c43096bc9
warlisonbezerra <warlisonbezerra1@gmail.com>
2023-12-03 16:37:40 -0300

    adicionando arquivo extensão python

---
+++ app.py
@@ -0,0 +1,34 @@
+#Compreendendo a variável na programação e seus tipos de dados.
+#Variável é a alocação de um dado na mémoria. Onde o valor a direita é alocado na variável 'velocidade_internet'.
+velocidade_internet = 10
+a,b,c,d = 15,3.15,-2,888
+#Tipos de dados:
+#Numéricos: 1,2,3;  -1,-22,-3;  3.15,3.85,5.654;
+velocidade_internet = 10
+#Booleanos: True, False (São Case sensitive);
+banda_larga = True
+#Strings: 'Sim', 'Não', '110 reais', "110 reais"
+eboaAnet = "Internet Boa"
+print(eboaAnet)
+print(a)
+print(b)
+print(c)
+print(d)
+
+#Descobrindo o tipo de cada dado:
+print(type(eboaAnet))
+print(type(a))
+print(type(banda_larga))
+
+#DESAFIO!
+#Qual é o tipo de dado das seguintes variáveis:
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = 'Bom dia'
+variavel_4 = 15
+
+#RESULTADO
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
